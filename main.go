3f47cc2806b4b1da9b600519425541a5155105d6
tkleg <thkleger@gmail.com>
2026-01-07 16:06:47 -0500

    switch statement built out

--- main.go
+++ main.go
@@ -9,13 +9,25 @@ func main(){
 		HighestID: 0,
 		CurrentIDs: make(map[int]struct{}),
 	}
-
-	tasklist.AddTask("Buy groceries")
-	tasklist.AddTask("Finish homework")
-	tasklist.AddTask("Call mom")
-	PrintTasks(&tasklist)
-
-	tasklist.MarkTaskDone(2)
-	PrintTasks(&tasklist)
-	
+	_ = tasklist // Suppress unused variable error
+	var command string
+	for {
+		fmt.Print("Enter command (add, done, list, help, exit): ")
+		fmt.Scan(&command)
+		switch command {
+			case "add":
+				fmt.Println("Adding")
+			case "done":
+				fmt.Println("Marking done")
+			case "list":
+				fmt.Println("Listing tasks")
+			case "help":
+				fmt.Println("Available commands: add, done, list, help, exit")
+			case "exit":
+				fmt.Println("Exiting Task Manager. Goodbye!")
+				return
+			default:
+				fmt.Println("Unknown command. Type 'help' for a list of commands.")
+		}
+	}
 }
