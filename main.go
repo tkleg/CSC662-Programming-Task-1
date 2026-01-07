fcbe90f0964a21bf4ea0436c87e15ffbe669da22
tkleg <thkleger@gmail.com>
2026-01-07 16:30:14 -0500

    basic functionality done

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"strconv"
+)
 
 func main(){
 	fmt.Println("Welcome to the Task Manager!")
@@ -9,20 +15,41 @@ func main(){
 		HighestID: 0,
 		CurrentIDs: make(map[int]struct{}),
 	}
-	_ = tasklist // Suppress unused variable error
+	scanner := bufio.NewReader(os.Stdin)
 	var command string
+	var taskDescription string
+	var taskID int
+	var taskIDStr string
+	var err error
+	_ = err
 	for {
-		fmt.Print("Enter command (add, done, list, help, exit): ")
-		fmt.Scan(&command)
+		fmt.Print("Enter command (add, done, list, delete, help, exit): ")
+		command, err = scanner.ReadString('\n')
+		command = command[:len(command)-1]
+		command = strings.TrimSpace(command)
 		switch command {
 			case "add":
-				fmt.Println("Adding")
+				fmt.Print("Enter task description: ")
+				taskDescription, err = scanner.ReadString('\n')
+				taskDescription = strings.TrimSpace(taskDescription)
+				tasklist.AddTask(taskDescription)
+				fmt.Println("Task added successfully!")
 			case "done":
-				fmt.Println("Marking done")
+				fmt.Print("Enter task ID to mark as done: ")
+				taskIDStr, err = scanner.ReadString('\n')
+				taskIDStr = strings.TrimSpace(taskIDStr)
+				taskID, err = strconv.Atoi(taskIDStr)
+				tasklist.MarkTaskDone(taskID)
 			case "list":
-				fmt.Println("Listing tasks")
+				PrintTasks(&tasklist)
 			case "help":
-				fmt.Println("Available commands: add, done, list, help, exit")
+				fmt.Println("Available commands: add, done, list, delete, help, exit")
+			case "delete":
+				fmt.Print("Enter task ID to delete: ")
+				taskIDStr, err = scanner.ReadString('\n')
+				taskIDStr = strings.TrimSpace(taskIDStr)
+				taskID, err = strconv.Atoi(taskIDStr)
+				tasklist.DeleteTask(taskID)
 			case "exit":
 				fmt.Println("Exiting Task Manager. Goodbye!")
 				return
